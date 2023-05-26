62a2c364c26a955f85b23660270f3e05d7e3966d
Ali Ghosn <alighosn2099@gmail.com>
2023-05-25 22:05:23 -0400

    Added rest of backend functionality

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,16 @@
 import User from "../mongodb/models/user.js";
 
-const getAllUsers = async (req, res) => {};
+const getAllUsers = async (req, res) => {
+    try {
+        const allUsers = await User.find({}).limit(req.query._end);
+
+        if(!allUsers) throw new Error({ message: 'Couldn\'t fetch users'  });
+
+        res.status(200).json(allUsers);
+    } catch (error) {
+        res.staus(500).json({ message: error.message });
+    }
+};
 
 const createUser = async (req, res) => {
     try{
@@ -20,6 +30,17 @@ const createUser = async (req, res) => {
 
 };
 
-const getUserInfoByID = async (req, res) => {};
+const getUserInfoByID = async (req, res) => {
+    const{ id } = req.params;
+
+    try {
+        const user = await User.findOne({ _id: id }).populate('allProperties');
+
+        if(user) return res.status(200).json(user);
+        throw new Error({ message: 'User not found' });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
 
 export { getAllUsers, createUser, getUserInfoByID };
